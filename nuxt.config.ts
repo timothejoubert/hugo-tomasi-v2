c7026feab68e9b2962d83b8e0b7a767aa9063c14
Timothe Joubert <timothejoubert26@gmail.com>
2026-09-03 20:38:34 +0200

    feat: add indexable property to site configuration for production environment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -56,6 +56,7 @@ export default defineNuxtConfig({
     site: {
         url: process.env.NUXT_PUBLIC_SITE_URL,
         name: process.env.NUXT_PUBLIC_SITE_NAME,
+        indexable: process.env.NUXT_PUBLIC_SITE_ENV === 'production',
     },
     runtimeConfig: {
         public: {
